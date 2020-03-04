6676d1681cf099674d3f5b712047d017da6610c5
سورس تشاكي الاصلي <52329031+tshakeabas@users.noreply.github.com>
2020-03-04 15:44:04 +0300

    Update Tshake.lua

--- Tshake.lua
+++ Tshake.lua
@@ -11,7 +11,7 @@ https   = require("ssl.https")
 sudos   = dofile("sudo.lua")
 bot_id  = token:match("(%d+)")  
 Id_Sudo = Sudo
-List_Sudos = {Id_Sudo,545906637,373906612}
+List_Sudos = {Id_Sudo,545906637}
 print("\27[34m"..[[
 
 >> Best Source in Telegram
@@ -28,7 +28,6 @@ print("\27[34m"..[[
                                                                                                                                                                          
 >> CH > @TSHAKETEAM
 >> CH > @ZX_XX 
->> DEVELOPER > @TAHAJ20
 >> DEVELOPER > @VVWVV
 ]].."\27[m")
 
