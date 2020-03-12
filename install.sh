a9abe2a03ded16530498994404188eed08b3aa9a
zxyu101 <42508744+zxyu101@users.noreply.github.com>
2020-03-12 22:56:56 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ chmod +x ts
 get() {
 rm -fr Tshake.lua
 rm -fr sudo.lua
-wget "https://raw.githubusercontent.com/tshakeabas/Tshake/master/Tshake.lua"
+wget "https://github.com/zxyu101/Tshake/blob/master/Tshake.lua"
 lua start.lua
 }
 installall(){
